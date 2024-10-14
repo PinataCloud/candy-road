404754df7db000e88181bb2e642a9cf09b955082
Steve <stevedsimkins@gmail.com>
2024-10-13 20:39:37 -0400

    added privy

---
+++ components/providers.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import { PrivyProvider } from "@privy-io/react-auth";
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+	return (
+		<PrivyProvider
+			appId={`${process.env.NEXT_PUBLIC_PRIVY_APP_ID}`}
+			config={{
+				// Customize Privy's appearance in your app
+				appearance: {
+					theme: "light",
+					accentColor: "#676FFF",
+					logo: "https://snippets.so/og.png",
+				},
+				loginMethods: ["wallet"],
+			}}
+		>
+			{children}
+		</PrivyProvider>
+	);
+}
