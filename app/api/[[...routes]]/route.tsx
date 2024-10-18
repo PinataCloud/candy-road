30f6a4575e4b59a02b19d4f0ccf976a3f518a6eb
Steve <stevedsimkins@gmail.com>
2024-10-18 15:12:43 -0400

    added address match check

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -4,7 +4,7 @@ import { handle } from "frog/next";
 import { pinata } from "@/utils/pinata";
 import { createClient } from "@/utils/supabase/server";
 import { validateFrameMessage } from "@/utils/verifyFrame";
-import { createPublicClient, http } from "viem";
+import { createPublicClient, http, isAddressEqual } from "viem";
 import { base } from "viem/chains";
 
 const publicClient = createPublicClient({
@@ -62,13 +62,20 @@ app.transaction("/purchase/:cid", async (c) => {
 
 app.frame("/complete/:cid", async (c) => {
 	const cid = c.req.param("cid");
+	const { data } = await pinata.gateways.get(cid);
+	const frameInfo = data as unknown as FrameCID;
 	const supabase = createClient();
 	const { transactionId } = c;
 	const transaction = await publicClient.waitForTransactionReceipt({
 		hash: transactionId!,
 	});
 
-	if (transaction.status !== "success") {
+	const addressMatch = isAddressEqual(
+		frameInfo.address as `0x`,
+		transaction.to as `0x`,
+	);
+
+	if (transaction.status !== "success" || !addressMatch) {
 		return c.res({
 			action: `/complete/${cid}`,
 			image:
