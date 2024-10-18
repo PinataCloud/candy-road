34e3d59cde8c3d02a7c0bf2f4f3e7be11a15803a
Steve <stevedsimkins@gmail.com>
2024-10-18 11:03:31 -0400

    added footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
+import { Footer } from "@/components/footer";
 import { FrameGrid } from "@/components/frame-grid";
 import { LoginButton } from "@/components/login-button";
 import { Nav } from "@/components/nav";
 
 export default function Home() {
 	return (
-		<div className="min-h-screen flex flex-col items-center gap-4 justify-start my-12 sm:mx-auto mx-2">
-			<Nav />
-			<LoginButton />
-			<FrameGrid />
+		<div className="min-h-screen flex flex-col items-center gap-4 justify-between my-12 sm:mx-auto mx-2">
+			<div className="flex flex-col items-center gap-4 justify-start sm:mx-auto mx-2 mb-8 w-full">
+				<Nav />
+				<LoginButton />
+				<FrameGrid />
+			</div>
+			<Footer />
 		</div>
 	);
 }
