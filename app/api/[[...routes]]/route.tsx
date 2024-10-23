7cecde80728ace798cc7277fd805e256c89454b0
Steve <stevedsimkins@gmail.com>
2024-10-23 15:37:05 -0400

    added sdk methods and tx verification

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -53,27 +53,27 @@ type FrameCID = {
 
 app.frame("/:cid", async (c) => {
 	const cid = c.req.param("cid");
-	// const { data } = await pinata.gateways.get(cid);
-	// console.log(data);
-	const signReq = await fetch("https://api.pinata.cloud/v3/files/sign", {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${process.env.PINATA_JWT}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			url: `https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/files/${cid}`,
-			date: Math.floor(new Date().getTime() / 1000),
-			expires: 30,
-			method: "GET",
-		}),
-	});
-	const signRes = await signReq.json();
-	console.log("sign res: %s, sign status: %s", signRes, signReq.status);
-	const dataReq = await fetch(signRes.data);
-	console.log("gateway request status: ", dataReq.status);
-	const data = await dataReq.json();
-	console.log("data from frame json: ", data);
+	const { data } = await pinata.gateways.get(cid);
+	console.log(data);
+	// const signReq = await fetch("https://api.pinata.cloud/v3/files/sign", {
+	// 	method: "POST",
+	// 	headers: {
+	// 		Authorization: `Bearer ${process.env.PINATA_JWT}`,
+	// 		"Content-Type": "application/json",
+	// 	},
+	// 	body: JSON.stringify({
+	// 		url: `https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/files/${cid}`,
+	// 		date: Math.floor(new Date().getTime() / 1000),
+	// 		expires: 30,
+	// 		method: "GET",
+	// 	}),
+	// });
+	// const signRes = await signReq.json();
+	// console.log("sign res: %s, sign status: %s", signRes, signReq.status);
+	// const dataReq = await fetch(signRes.data);
+	// console.log("gateway request status: ", dataReq.status);
+	// const data = await dataReq.json();
+	// console.log("data from frame json: ", data);
 
 	const frameInfo = data as unknown as FrameCID;
 	return c.res({
@@ -93,27 +93,27 @@ app.frame("/:cid", async (c) => {
 
 app.transaction("/purchase/:cid", async (c) => {
 	const cid = c.req.param("cid");
-	// const { data } = await pinata.gateways.get(cid);
-	// console.log(data);
-	const signReq = await fetch("https://api.pinata.cloud/v3/files/sign", {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${process.env.PINATA_JWT}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			url: `https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/files/${cid}`,
-			date: Math.floor(new Date().getTime() / 1000),
-			expires: 30,
-			method: "GET",
-		}),
-	});
-	const signRes = await signReq.json();
-	console.log("sign res: %s, sign status: %s", signRes, signReq.status);
-	const dataReq = await fetch(signRes.data);
-	console.log("gateway request status: ", dataReq.status);
-	const data = await dataReq.json();
-	console.log("data from frame json: ", data);
+	const { data } = await pinata.gateways.get(cid);
+	console.log(data);
+	// const signReq = await fetch("https://api.pinata.cloud/v3/files/sign", {
+	// 	method: "POST",
+	// 	headers: {
+	// 		Authorization: `Bearer ${process.env.PINATA_JWT}`,
+	// 		"Content-Type": "application/json",
+	// 	},
+	// 	body: JSON.stringify({
+	// 		url: `https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/files/${cid}`,
+	// 		date: Math.floor(new Date().getTime() / 1000),
+	// 		expires: 30,
+	// 		method: "GET",
+	// 	}),
+	// });
+	// const signRes = await signReq.json();
+	// console.log("sign res: %s, sign status: %s", signRes, signReq.status);
+	// const dataReq = await fetch(signRes.data);
+	// console.log("gateway request status: ", dataReq.status);
+	// const data = await dataReq.json();
+	// console.log("data from frame json: ", data);
 
 	const frameInfo = data as unknown as FrameCID;
 
@@ -146,30 +146,30 @@ app.frame("/complete/:cid/:tx?", async (c) => {
 		});
 	}
 
-	// const { data } = await pinata.gateways.get(cid);
-	// const frameInfo = data as unknown as FrameCID;
-	// const txInfo = await publicClient.getTransaction({ hash: pendingTx });
+	const { data } = await pinata.gateways.get(cid);
+	const frameInfo = data as unknown as FrameCID;
+	const txInfo = await publicClient.getTransaction({ hash: pendingTx });
 
-	// const validAddress = isAddressEqual(
-	// 	frameInfo.address as `0x`,
-	// 	txInfo.to as `0x`,
-	// );
-	// console.log("address check: ", validAddress);
-	// const value = formatEther(txInfo.value);
-	// console.log("tx check: ", value);
+	const validAddress = isAddressEqual(
+		frameInfo.address as `0x`,
+		txInfo.to as `0x`,
+	);
+	console.log("address check: ", validAddress);
+	const value = formatEther(txInfo.value);
+	console.log("tx check: ", value);
 
-	// if (!validAddress || frameInfo.price !== value) {
-	// 	return c.res({
-	// 		action: `/complete/${cid}`,
-	// 		image:
-	// 			"https://cdn.candyroad.cloud/files/bafkreid2oevexyvxl5gmcv7t52ke5bwxzswjhbqmrtqho3hk4srwz6wxse?filename=unauthorized.png",
-	// 		intents: [
-	// 			<Button.Transaction key="1" target={`/purchase/${cid}`}>
-	// 				{frameInfo.price} Ξ
-	// 			</Button.Transaction>,
-	// 		],
-	// 	});
-	// }
+	if (!validAddress || frameInfo.price !== value) {
+		return c.res({
+			action: `/complete/${cid}`,
+			image:
+				"https://cdn.candyroad.cloud/files/bafkreid2oevexyvxl5gmcv7t52ke5bwxzswjhbqmrtqho3hk4srwz6wxse?filename=unauthorized.png",
+			intents: [
+				<Button.Transaction key="1" target={`/purchase/${cid}`}>
+					{frameInfo.price} Ξ
+				</Button.Transaction>,
+			],
+		});
+	}
 
 	const { data: insertRes, error } = await supabase
 		.from("purchases")
@@ -233,26 +233,26 @@ app.frame("/redeem/:cid", async (c) => {
 	const verification = await validateFrameMessage(body);
 	const cid = c.req.param("cid");
 	const supabase = createClient();
-	//const { data } = await pinata.gateways.get(cid);
-	const signReq = await fetch("https://api.pinata.cloud/v3/files/sign", {
-		method: "POST",
-		headers: {
-			Authorization: `Bearer ${process.env.PINATA_JWT}`,
-			"Content-Type": "application/json",
-		},
-		body: JSON.stringify({
-			url: `https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/files/${cid}`,
-			date: Math.floor(new Date().getTime() / 1000),
-			expires: 30,
-			method: "GET",
-		}),
-	});
-	const signRes = await signReq.json();
-	console.log("sign res: %s, sign status: %s", signRes, signReq.status);
-	const dataReq = await fetch(signRes.data);
-	console.log("Gateway fetch status: ", dataReq.status);
-	const data = await dataReq.json();
-	console.log("data from frame json: ", data);
+	const { data } = await pinata.gateways.get(cid);
+	// const signReq = await fetch("https://api.pinata.cloud/v3/files/sign", {
+	// 	method: "POST",
+	// 	headers: {
+	// 		Authorization: `Bearer ${process.env.PINATA_JWT}`,
+	// 		"Content-Type": "application/json",
+	// 	},
+	// 	body: JSON.stringify({
+	// 		url: `https://${process.env.NEXT_PUBLIC_GATEWAY_URL}/files/${cid}`,
+	// 		date: Math.floor(new Date().getTime() / 1000),
+	// 		expires: 30,
+	// 		method: "GET",
+	// 	}),
+	// });
+	// const signRes = await signReq.json();
+	// console.log("sign res: %s, sign status: %s", signRes, signReq.status);
+	// const dataReq = await fetch(signRes.data);
+	// console.log("Gateway fetch status: ", dataReq.status);
+	// const data = await dataReq.json();
+	// console.log("data from frame json: ", data);
 	const frameInfo = data as unknown as FrameCID;
 
 	const { data: rows, error } = await supabase
